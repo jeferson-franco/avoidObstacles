e1f493e52b274f11b52945aef536440435e55d71
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:24:59 -0300

    update avoidObstacles.js

--- src/avoidObstacles.js
+++ src/avoidObstacles.js
@@ -1,6 +1,6 @@
 function solution(inputArray) {
-    for(let i = 1; ; i++) {
-        if(inputArray.every(element => element % i !== 0)) {
+    for (let i = 1; ; i++) {
+        if (inputArray.every((element) => element % i !== 0)) {
             return i;
         }
     }
@@ -12,3 +12,20 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test avoidObstacles
+
+// alternative solution
+// function solution(arr) {
+//     for (let n = 1; ; n++) if (arr.every((x) => x % n)) return n;
+// }
+
+// alternative solution
+// function solution(inputArray) {
+//     let jump = 2;
+//     while (inputArray.some((obstacle) => obstacle % jump == 0)) {
+//         jump++;
+//     }
+//     return jump;
+// }
+
+// alternative solution
+// A = solution = (a, j) => ((j = -~j), a.every((e) => e % j) ? j : A(a, j));
